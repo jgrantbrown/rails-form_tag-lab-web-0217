f47d14c239de06d120882f1e6e4e5030b266414d
Your Name <you@example.com>
2017-03-01 13:49:27 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
-  get '/student/:id', to: 'students#show', as: 'student'
+  resources :students, only: [:index, :show, :create, :new]
+  # get '/student/:id', to: 'students#show', as: 'student'
+  # get '/student/new', to: 'students#new'
+  # get '/student/create', to: 'students#create'
 end
